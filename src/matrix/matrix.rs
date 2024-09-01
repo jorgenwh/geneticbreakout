ae3aa32a027dfd862f505c5acc7e06558f9f0db0
jorgenwh <jorgenwhen@gmail.com>
2024-09-01 23:01:36 +0200

    added matrix ops and implemented matmul. need to create tests, but probably create zeros, ones and arange initializers first.

--- src/matrix/matrix.rs
+++ src/matrix/matrix.rs
@@ -1,4 +1,4 @@
-#[allow(dead_code)]
+#[derive(Debug, PartialEq)]
 pub struct Matrix {
     rows: usize,
     cols: usize,
--- tests/test_matrix.rs
+++ tests/test_matrix.rs
@@ -1,4 +1,5 @@
 use geneticbreakout::matrix::matrix::Matrix;
+use geneticbreakout::matrix::ops;
 
 #[test]
 fn test_data() {
@@ -24,4 +25,37 @@ fn test_shape() {
     assert_eq!(m.rows(), 2);
     assert_eq!(m.cols(), 3);
     assert_eq!(m.shape(), (2, 3));
+}
+
+#[test]
+fn test_eq() {
+    let mut m1 = Matrix::new(2, 3);
+    let mut m2 = Matrix::new(2, 3);
+    assert!(m1 == m1);
+    assert!(m1 == m2);
+
+    m1.set(0, 0, 1.0);
+    assert!(m1 == m1);
+    assert!(m1 != m2);
+
+    m2.set(0, 0, 1.0);
+    assert!(m1 == m2);
+
+    let mut m1 = Matrix::new(2, 3);
+    let mut m2 = Matrix::new(3, 2);
+    assert!(m1 != m2);
+
+    m1.set(0, 0, 1.0);
+    m2.set(0, 0, 1.0);
+    assert!(m1 != m2);
+}
+
+#[test]
+fn test_matmul() {
+    let mut a = Matrix::new(2, 3);
+    for i in 0..2 {
+        for j in 0..3 {
+            a.set(i, j, (i * 3 + j) as f32);
+        }
+    }
 }
